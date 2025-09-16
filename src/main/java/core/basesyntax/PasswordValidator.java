9a6bd67e1f102793bc6bd02d32710219399ff079
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-09-16 10:02:05 +0300

    done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        int MIN_LENGTH = 10;
+    public static final int MIN_LENGTH = 10;
+
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         boolean isPasswordEquals = password.equals(repeatPassword);
 
         if (!isPasswordEquals || password.length() < MIN_LENGTH) {
